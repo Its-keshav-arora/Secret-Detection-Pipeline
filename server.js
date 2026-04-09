62db3ebd6acff00db427695bfbc3a6498497c85d
keshav arora <keshav11y@gmail.com>
2026-04-09 19:19:59 +0530

    security update

--- server.js
+++ server.js
@@ -2,10 +2,6 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-// AWS_ACCESS_KEY_ID=AKIAIOSFODNN7EXAMPLE
-// AWS_SECRET_ACCESS_KEY=wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY
-// DATABASE_URL=postgres://admin:supersecret123@db.internal:5432/prod
-
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
